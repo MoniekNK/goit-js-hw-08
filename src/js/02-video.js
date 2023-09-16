d70607a7cdb0b48a89d64b080fbcfc5fcc7868db
MoniekNK <monika.nowakowska01@gmail.com>
2023-09-16 13:24:25 +0200

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const vimeoIframe = document.querySelector('vimeo-player');
+const vimeoIframe = document.querySelector('#vimeo-player'); // Dodaj "#" przed id
 const player = new Player(vimeoIframe);
 
 const handleTimeUpdateThrottled = throttle(event => {
   const currentTime = event.seconds;
-  //console.log(currentTime);
   localStorage.setItem(VIDEO_STORAGE_KEY, currentTime.toString());
 }, 1000);
 
 const VIDEO_STORAGE_KEY = 'videoplayer-current-time';
 
-player.on('timeupdate', saveTimeToLocalStorage);
+// Zdefiniuj funkcję saveTimeToLocalStorage
+function saveTimeToLocalStorage(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem(VIDEO_STORAGE_KEY, currentTime.toString());
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const savedTime = localStorage.getItem(VIDEO_STORAGE_KEY);
